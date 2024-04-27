9c45c03660e6ecc58bf5abb4c40af600c0da6efc
martahinojosa1 <martahinojosa.001@gmail.com>
2024-04-27 17:03:04 +0200

    predictor run

--- Streamlit_app_Feverup/pages/3-Price_predictor.py
+++ Streamlit_app_Feverup/pages/3-Price_predictor.py
@@ -2,9 +2,8 @@
 import joblib   # añadir a requirements
 import streamlit as st
 
-
 # Carga el modelo desde el archivo .pkl
-modelo = joblib.load("modelos\Extra_Trees_Regressor.pkl")
+modelo = joblib.load("modelos\Gradient_Boosting_Regressor.pkl")
 
 
 # Título predictor
@@ -12,12 +11,17 @@ st.title('Ticket price predictor')
 
 # Widgets para la entrada de datos
 event_category = st.selectbox('Event Category', options=['THEATER', 'TASTING', 'IGHTLIF', 'CONCERT', 'WELLNESS', 'MIX'])
-country = st.selectbox('Country', options=['GB', 'US', 'CA'])
+city = st.selectbox('City', options=['London', 'New York', 'Chicago', 'Dallas', 'Birmingham', 'Austin', 'Edmonton'])
 
 # Botón para realizar la predicción
 if st.button('Predecir'):
     # Realiza la predicción utilizando el modelo cargado
-    precio_predicho = modelo.predict([[event_category, country]])[0]
+    data = {
+    'city': [city],            
+    'event_category': [event_category],   
+}
+    precio_predicho = round(modelo.predict(data)[0],2)
+    print(precio_predicho) 
     st.write(f'El precio predicho es: {precio_predicho}')
 
 
